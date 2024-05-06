260e50ccb80877dcda1bbf3b550c73e76665bb81
Anton <80984465+Antongo22@users.noreply.github.com>
2024-05-06 23:50:12 +0300

    начало pdf

---
+++ Struct/ChaptersPDF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaReader.Struct
+{
+    internal class ChaptersPDF : Chapter
+    {
+        public ChaptersPDF(Manga parentManga, string chapterName, string pathToChapter) : base(parentManga, chapterName, pathToChapter)
+        {
+
+        }
+    }
+}
